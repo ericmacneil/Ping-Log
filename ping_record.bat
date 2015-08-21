a82ca90ca6344688f7a135ae59e6fbdc179cae39
ericmacneil <emacneil@gmail.com>
2015-08-21 09:02:30 -0400

    Logs ping time to text file
    Logs ping time to text file.
    
    Default time between pings 3 sec
    Default ping server 127.0.0.1.
    Default text file pinglog.txt
    
    Default behavior is to append data to end of text file every time it is
    run.

---
+++ ping_record.bat
@@ -0,0 +1,5 @@
+@echo off
+for /f "tokens=*" %%A in ('ping 127.0.0.1 -n 1 ') do (echo %%A>>pinglog.txt && GOTO Ping)
+:Ping
+timeout /t 3 /nobreak
+for /f "tokens=* skip=2" %%A in ('ping 127.0.0.1 -n 1 ') do (echo %date% %time% %%A>>pinglog.txt  && GOTO Ping)
